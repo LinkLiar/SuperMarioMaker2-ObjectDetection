736bc9ae7571f5c356a7f8cdacda7526f0308485
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-12 23:56:44 +0800

    Update VideoTest.py

--- VideoTest.py
+++ VideoTest.py
@@ -3,6 +3,8 @@ import paho.mqtt.client as mqtt
 import time
 import sys
 import cv2
+import numpy as np
+
 width = 1920
 height = 1080
 
@@ -14,14 +16,23 @@ def SendingImage():
     print("Camera initialized successfully...")
     sum = 0
     count = 0
+
+    # set the lower and upper bounds for the green hue
+    lower_green = np.array([10, 43, 46])
+    upper_green = np.array([11, 255, 255])
+
     while True:
         ret, frame = cap.read()
         start = cv2.getTickCount()
         result = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25,
                             interpolation=cv2.INTER_NEAREST)
+
+        hsvImage = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsvImage, lower_green, upper_green)
+
         end = cv2.getTickCount()
         during = (end - start) / cv2.getTickFrequency()
-
+        cv2.imshow("Color Filter", mask)
         sum += during
         count += 1
         print(sum/count)
