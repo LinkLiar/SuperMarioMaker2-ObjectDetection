b534a2297263a1250a86f1d384a61f42bbf7eb3e
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-13 00:56:24 +0800

    update

--- VideoTest.py
+++ VideoTest.py
@@ -1,12 +1,25 @@
-from unittest import result
-import paho.mqtt.client as mqtt
 import time
 import sys
 import cv2
 import numpy as np
+from numba import jit
 
 width = 1920
 height = 1080
+
+
+@jit
+def FilterColor(img, color):
+    info = img.shape
+    height = info[0]
+    width = info[1]
+    mask = np.zeros((height, width, 1), np.uint8)
+    for h in range(0, height):
+        for j in range(0, width):
+            (b, g, r) = img[h, j]
+            if (b, g, r) == color:
+                mask[h, j] = 255
+    return mask
 
 
 def SendingImage():
@@ -17,19 +30,16 @@ def SendingImage():
     sum = 0
     count = 0
 
-    # set the lower and upper bounds for the green hue
-    lower_green = np.array([10, 43, 46])
-    upper_green = np.array([11, 255, 255])
-
+    RED = (14, 57, 247)  # Red
+    GREEN = (45, 145, 27)  # Green
+    BLUE = (255, 80, 81)  # Blue
+    PINK = (198, 80, 255)  # Pink
     while True:
         ret, frame = cap.read()
         start = cv2.getTickCount()
         result = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25,
                             interpolation=cv2.INTER_NEAREST)
-
-        hsvImage = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsvImage, lower_green, upper_green)
-
+        mask = FilterColor(result, GREEN)
         end = cv2.getTickCount()
         during = (end - start) / cv2.getTickFrequency()
         cv2.imshow("Color Filter", mask)
