83290f7c26588bceaec0976f6907f3b3b62b4629
Link <38213500+LinkLiar@users.noreply.github.com>
2022-04-16 22:33:49 +0800

    update

--- ObjectImage/VideoTest.py
+++ ObjectImage/VideoTest.py
@@ -9,16 +9,18 @@ height = 1080
 
 
 @jit
-def FilterColor(img, color):
+def FilterColor(img, colors):
     info = img.shape
     height = info[0]
     width = info[1]
     mask = np.zeros((height, width, 1), np.uint8)
     for h in range(0, height):
         for j in range(0, width):
-            (b, g, r) = img[h, j]
-            if (b, g, r) == color:
-                mask[h, j] = 255
+            probeColor = img[h, j]
+            for color in colors:
+                if(probeColor[0] == color[2] and probeColor[1] == color[1] and probeColor[2] == color[0]):
+                    mask[h, j] = 255
+                    break
     return mask
 
 
@@ -30,16 +32,28 @@ def SendingImage():
     sum = 0
     count = 0
 
-    RED = (14, 57, 247)  # Red
-    GREEN = (45, 145, 27)  # Green
-    BLUE = (255, 80, 81)  # Blue
-    PINK = (198, 80, 255)  # Pink
+    SuperMarioBro_RED = (14, 57, 247)  # Red
+    SuperMarioBro_GREEN = (45, 145, 27)  # Green
+    SuperMarioBro_BLUE = (255, 80, 81)  # Blue
+    SuperMarioBro_PINK = (198, 80, 255)  # Pink
+
+    SuperMarioBro3_Body = (178, 217, 255)
+    SuperMarioBro3_Rect = (31, 31, 31)
+
+    # Red,(231, 61, 18),(209, 66, 34), (157, 81, 41), (132, 85, 74)(247, 57, 14),
+    SuperMarioBro3_RED = ((231, 61, 18))
+    #   (209, 66, 34), (157, 81, 41), (132, 85, 74))
+    SuperMarioBro3_RED_InWater = (15, 38, 109)  # Red
+    SuperMarioBro3_GREEN = (45, 145, 27)  # Green
+    SuperMarioBro3_BLUE = (255, 80, 81)  # Blue
+    SuperMarioBro3_PINK = (198, 80, 255)  # Pink
+
     while True:
         ret, frame = cap.read()
         start = cv2.getTickCount()
         result = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25,
                             interpolation=cv2.INTER_NEAREST)
-        mask = FilterColor(result, GREEN)
+        mask = FilterColor(result, SuperMarioBro3_RED)
         end = cv2.getTickCount()
         during = (end - start) / cv2.getTickFrequency()
         cv2.imshow("Color Filter", mask)
@@ -49,7 +63,7 @@ def SendingImage():
         cv2.imshow("Test Frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord(' '):
-            cv2.imwrite(f"{count}.png", result)
+            cv2.imwrite(f"{count}.png", frame)
             print("Save Successfully...")
 
 
