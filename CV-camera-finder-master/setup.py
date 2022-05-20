8cd8fbe579007f04da13e1b018cda8a28484e5ca
Link <38213500+LinkLiar@users.noreply.github.com>
2022-05-20 23:57:39 +0800

    update

--- CV-camera-finder-master/setup.py
+++ CV-camera-finder-master/setup.py
@@ -1,5 +1,3 @@
-__author__ = 'Lika'
-
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -9,16 +7,17 @@ from Cython.Build import cythonize
 sourcefiles = ['pymf.cpp', 'cap.cpp']
 
 ext_modules = [
-#cythonize("pymf.cpp")
-# V2 Beta
-Extension("pymf", sourcefiles,
-include_dirs=["capture"],
-libraries=["ole32"], #error LNK2001: unresolved external symbol __imp_CoTaskMemFree
-language="c++"),
+    # cythonize("pymf.cpp")
+    # V2 Beta
+    Extension("pymf", sourcefiles,
+              include_dirs=["capture"],
+              # error LNK2001: unresolved external symbol __imp_CoTaskMemFree
+              libraries=["ole32"],
+              language="c++"),
 ]
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules= cythonize(
-            ext_modules,
-            compiler_directives={'language_level' : "3"}),
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={'language_level': "3"}),
 )
